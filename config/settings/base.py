a07ef372be18818eb67d56775e3a8be471a64301
chulwan.lim <limchulwan132@gmail.com>
2019-03-26 00:30:09 +0900

    create notification

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,7 +80,8 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'instagram.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
-    'instagram.images.apps.ImagesConfig',
+    'instagram.images.apps.ImagesConfig', # Images app
+    'instagram.notifications.apps.NotificationsConfig' # notifications app
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
