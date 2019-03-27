cd91055d813b164d06c52d55ada6e13d11c85223
chulwan.lim <limchulwan132@gmail.com>
2019-03-28 00:49:00 +0900

    moderating comment on my image

--- instagram/images/views.py
+++ instagram/images/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from instagram.notifications import views as notification_views
 
 class Feed(APIView):
 
@@ -64,8 +65,6 @@ class LikeImage(APIView):
 
         user = request.user
 
-        # create notification for like
-
         try:
             found_image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
@@ -87,6 +86,8 @@ class LikeImage(APIView):
             )
 
             new_like.save()
+
+            notification_views.create_notification(user, found_image.creator, 'like', found_image)
 
             return Response(status=status.HTTP_201_CREATED)
 
@@ -120,8 +121,6 @@ class CommentOnImage(APIView):
 
         user = request.user
 
-        # comment notification
-
         try:
             found_image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
@@ -132,6 +131,8 @@ class CommentOnImage(APIView):
         if serializer.is_valid():
 
             serializer.save(creator=user, image=found_image)
+
+            notification_views.create_notification(user, found_image.creator, 'comment', found_image, serializer.data['message'])
 
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
@@ -172,3 +173,23 @@ class Search(APIView):
         else:
 
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class ModerateComments(APIView):
+
+    def delete(self, request, image_id, comment_id, format=None):
+
+        user = request.user
+
+        # try:
+        #     image = models.Image.objects.get(id=image_id, creator=user)
+        # except models.Image.DoesNotExist:
+        #     return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            comment_to_delete = models.Comment.objects.get(id=comment_id, image__id=image_id, image__creator=user)
+            comment_to_delete.delete()
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
